84c15ad598ae8375f4457d13aa0ea36611e2507d
feng <feng2860984180@163.com>
2022-08-29 19:13:06 +0800

    babel 编译器原理

--- 07-webpack-babel的使用/src/index.js
+++ 07-webpack-babel的使用/src/index.js
@@ -5,3 +5,14 @@ const foo = (info) => {
 }
 
 foo(message)
+
+// 1. 原生代码
+// 2. 词法分析 Lexical Analysis（关键字、标识符...）
+// 3. 生成 tokens 数组，针对代码每一个值进行分类 如 { type: 'Keyword', value: 'const' }
+// 4. 语法分析（syntactic analysis）
+// 5. AST（abstract syntax tree）抽象语法树
+// 6. 遍历（Traversal）
+// 7. 访问（Visitor）
+// 8. 应用插件（Plugin）
+// 9. 新的 AST
+// 10. 生成目标代码
