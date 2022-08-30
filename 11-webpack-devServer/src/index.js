70f5ccb21c8d3040f1efce27490563bd79327fb1
feng <feng2860984180@163.com>
2022-08-31 02:08:36 +0800

    React 通过 react-refresh 的 HMR

--- 11-webpack-devServer/src/index.js
+++ 11-webpack-devServer/src/index.js
@@ -1,9 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import './math'
+import ReactApp from './App.jsx'
 
 console.log('Hello Webpack')
 
-module.hot &&
+if (module.hot) {
   module.hot.accept('./math.js', () => {
     console.log(module)
     console.log('math 模块发生了热更新')
   })
+}
+
+// React
+ReactDOM.render(<ReactApp />, document.querySelector('#app'))
--- 11-webpack-devServer/webpack.config.js
+++ 11-webpack-devServer/webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
-  mode: 'none',
+  mode: 'development',
   // 默认为 false
   // true：开启代码监听
   // watch: true,
 
   entry: './src/index.js',
+
+  devtool: 'cheap-module-source-map',
 
   // 专为 webpack-dev-server 的配置
   devServer: {
@@ -19,10 +22,28 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            // 配置 React HMR
+            plugins: ['react-refresh/babel'],
+          },
+        },
+      },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       title: 'webpack title',
       template: './index.html',
     }),
+    // 配置 React HMR
+    new ReactRefreshWebpackPlugin(),
   ],
 }
