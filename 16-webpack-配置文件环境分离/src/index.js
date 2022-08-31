fe47d113c3cf531c86e0adcc42d33332be1ffa7c
feng <feng2860984180@163.com>
2022-08-31 21:18:26 +0800

    复制了一份目录文件

---
+++ 16-webpack-配置文件环境分离/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createApp } from 'vue'
+
+import ReactApp from './App.jsx'
+import VueApp from './App.vue'
+
+console.log('Hello Webpack')
+
+// React
+ReactDOM.render(<ReactApp />, document.querySelector('#app'))
+
+// Vue
+const app = createApp(VueApp)
+
+app.mount('#vue')
