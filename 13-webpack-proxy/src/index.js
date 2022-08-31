2aaf0251b0741abe2727351cc84ca1b81dcbf288
feng <feng2860984180@163.com>
2022-08-31 17:51:29 +0800

    devServer 中的 proxy 配置

--- 13-webpack-proxy/src/index.js
+++ 13-webpack-proxy/src/index.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 
 import VueApp from './App.vue'
 
@@ -7,3 +8,24 @@ const app = createApp(VueApp)
 app.mount('#vue')
 
 console.log('Hello Webpack')
+
+// axios
+if (0) {
+  axios
+    .get('https://httpbin.org/ip')
+    .then((res) => {
+      console.log('res', res)
+    })
+    .catch((err) => {
+      console.log('error', err)
+    })
+}
+
+axios
+  .get('/chen/ip', { params: { name: 'code' } })
+  .then((res) => {
+    console.log('res', res)
+  })
+  .catch((err) => {
+    console.log('error', err)
+  })
--- 13-webpack-proxy/webpack.config.js
+++ 13-webpack-proxy/webpack.config.js
@@ -17,12 +17,27 @@ module.exports = {
   devServer: {
     // host: '0.0.0.0',
     // port: 8080,
-    open: true,
+    // open: true,
     // compress: true,
     hot: 'only',
     // publicPath: '',
     // contentBase: path.resolve(__dirname, './')
     // watchContentBase: true,
+    // 请求代理：只在本地开发有效，部署后无效
+    proxy: {
+      '/chen': {
+        // 需要代理的服务器地址J
+        // 这样的话 /chen/ip 的请求会将请求代理到 https://httpbin.org/chen/ip
+        target: 'https://httpbin.org',
+        // 替换字符串/标识字符
+        // 将实际请求链接中的 /chen 替换为空字符串，因为实际的的接口可能没有这个标识字符
+        pathRewrite: { '^/chen': '' },
+        // 默认情况下，将不接受在 HTTPS 上运行且证书无效的后端服务器，如果需要：设置为 false
+        secure: false,
+        // 让服务器收到的请求头中 host 是上面的目标地址 target，默认情况下是本地服务的 host+port
+        changeOrigin: true,
+      },
+    },
   },
 
   module: {
