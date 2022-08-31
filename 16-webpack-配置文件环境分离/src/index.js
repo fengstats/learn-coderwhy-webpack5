e8aed3d817fd1c5fd99197992ec19c161d580935
feng <feng2860984180@163.com>
2022-08-31 21:24:36 +0800

    根据学习内容删除掉一些干扰文件

--- 16-webpack-配置文件环境分离/src/index.js
+++ 16-webpack-配置文件环境分离/src/index.js
@@ -1,16 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createApp } from 'vue'
-
-import ReactApp from './App.jsx'
-import VueApp from './App.vue'
-
-console.log('Hello Webpack')
-
-// React
-ReactDOM.render(<ReactApp />, document.querySelector('#app'))
-
-// Vue
-const app = createApp(VueApp)
-
-app.mount('#vue')
+console.log('Hello Webpack 111 222')
--- 16-webpack-配置文件环境分离/webpack.config.js
+++ 16-webpack-配置文件环境分离/webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { VueLoaderPlugin } = require('vue-loader/dist/index')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -13,21 +11,13 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
   },
 
-  // 开发服务
   devServer: {
     hot: true,
-    // 解决路由模式为 history 时，切换路由后刷新页面 404 问题
     historyApiFallback: true,
   },
 
-  // 用于设置模块如何被解析
   resolve: {
-    // 引入模块从哪些目录进行检索查找
-    // modules: ['node_module']
-    // 自动填充后缀：可以让我们引入资源文件时省略后缀的编写，webpack 会根据这个配置项中的后缀一一进行匹配，若未匹配到则报错
-    // tip: 建议在文件引入时还是要添加上后缀的，一来可以省去 webpack 的匹配查找时间，二来自己看代码的时候也知道引入的到底是什么类型的文件
     extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx', '.ts', '.vue'],
-    // 配置别名：顾名思义，其实就是替代一下相对路径引入的写法，提前帮你把绝对路径配置好了
     alias: {
       '@': path.resolve(__dirname, 'src'),
       pages: path.resolve(__dirname, 'src/pages'),
@@ -35,28 +25,7 @@ module.exports = {
   },
 
   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.vue$/,
-        // vue-loader 加载的组件默认会帮助我们进行 HMR 的处理
-        use: 'vue-loader',
-      },
-      {
-        test: /\.jsx?$/i,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            // 配置 React HMR
-            plugins: ['react-refresh/babel'],
-          },
-        },
-      },
-    ],
+    rules: [],
   },
 
   plugins: [
@@ -64,8 +33,5 @@ module.exports = {
       title: 'webpack title',
       template: './index.html',
     }),
-    // 配置 React HMR
-    new ReactRefreshWebpackPlugin(),
-    new VueLoaderPlugin(),
   ],
 }
