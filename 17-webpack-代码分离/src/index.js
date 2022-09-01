37a8992646886ffbb87364004b76ac7d3178b9eb
feng <feng2860984180@163.com>
2022-09-01 19:46:44 +0800

    import 函数引入的文 webpack 都会进行代码分离

--- 17-webpack-代码分离/src/index.js
+++ 17-webpack-代码分离/src/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash'
 import dayjs from 'dayjs'
 
-import code from './code_1.js'
+// import code from './code_1.js'
 
 console.log(_.join(['hello', 'index']))
 console.log('main', dayjs())
 
 // import 函数异步加载模块，通过 splitChunks.chunks 属性来设置是否处理
-import('lodash').then((res) => {
-  console.log('lodash')
-})
+// import('lodash').then((res) => {
+//   console.log('lodash')
+// })
--- 17-webpack-代码分离/src/main.js
+++ 17-webpack-代码分离/src/main.js
@@ -1,6 +1,14 @@
 // import _ from 'lodash'
 // import dayjs from 'dayjs'
-import code from './code_1.js'
 
 // console.log(_.join(['hello', 'main']))
 // console.log('index', dayjs())
+
+// 只要是异步引入的代码，webpack 都会进行代码分离
+import('./bar.js').then((res) => {
+  console.log(res)
+})
+
+import('./code_1.js').then((res) => {
+  console.log(res)
+})
