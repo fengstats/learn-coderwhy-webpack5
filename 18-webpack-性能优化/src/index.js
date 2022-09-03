67fec96e73882897f3e470f3eef1786ec1f31de5
feng <feng2860984180@163.com>
2022-09-03 20:50:59 +0800

    prefetch 和 preload

--- 18-webpack-性能优化/src/index.js
+++ 18-webpack-性能优化/src/index.js
@@ -10,7 +10,14 @@ const btn = document.createElement('button')
 btn.innerHTML = '加载 Element 内容'
 
 btn.onclick = () => {
-  import('./module/element.js').then((res) => {
+  import(
+    /* webpackChunkName: 'element' */
+    // 文件预加载：低优先级，在浏览器空闲时下载文件，等待加载时直接使用 prefetch cache 加载文件（提升用户体验）
+    /* webpackPrefetch: true */
+    // 相对比 prefetch 来说，具有中等优先级，会在父 chunk 加载时，以并行的方式下载
+    /* webpackPreload: true */
+    './module/element.js'
+  ).then((res) => {
     console.log('element', res)
     const { default: element } = res
     console.log(element)
