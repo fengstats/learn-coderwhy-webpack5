26b720d6ad071f0462ed6f2dbedcf1efcdfddffa
feng <feng2860984180@163.com>
2022-09-04 23:32:52 +0800

    Tree Shaking 方案一-usedExports&Terser

---
+++ 21-webpack-性能优化-Tree-Shaking/src/main.js
@@ -0,0 +1,6 @@
+// import './index.css'
+import { sum, mul } from './math'
+
+// console.log(obj.say())
+console.log(sum(1, 2))
+// console.log(mul(1, 2))
---
+++ 21-webpack-性能优化-Tree-Shaking/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  optimization: {
+    // 将未使用的代码添加一段注释标记
+    usedExports: true,
+    // 是否开启压缩优化 Terser
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          // mangle: true,
+          // toplevel: true,
+          // keep_classnames: true,
+          // compress: {
+          //   arguments: true,
+          //   arrows: true,
+          // },
+        },
+      }),
+    ],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+    }),
+    new CssMinimizerWebpackPlugin(),
+    // new webpack.optimize.ModuleConcatenationPlugin(),
+  ],
+}
