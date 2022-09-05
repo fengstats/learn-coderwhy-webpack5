850337a83e760e4f21af350109fc3aa61791c16a
feng <feng2860984180@163.com>
2022-09-06 00:03:15 +0800

    将 runtimeChunk 的数据打包至 html 中

--- 22-webpack-性能优化-HTTP-HTML压缩/webpack.config.js
+++ 22-webpack-性能优化-HTTP-HTML压缩/webpack.config.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
+const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin')
 
 // 测试使用
 // let mode = 'development'
@@ -36,6 +37,9 @@ module.exports = {
         },
       }),
     ],
+    runtimeChunk: {
+      name: 'runtime',
+    },
   },
 
   module: {
@@ -56,8 +60,38 @@ module.exports = {
     new CssMinimizerWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Tree Shaking',
-      // 设置模板
+      // 设置 html 模板
       template: './index.html',
+      // 控制 css/js 文件要不要注入到 html 中，注入到哪个位置
+      // head | body
+      inject: true,
+      // 当文件没有发生任何改变时，使用之前的缓存
+      cache: true,
+      // 压缩优化
+      minify:
+        mode === 'production'
+          ? {
+              // 是否要移除 html 中的注释信息
+              removeComments: true,
+              // 是否移除一些多余属性：如 <input type="text">，type 默认值就是 tex，可以移除
+              removeRedundantAttributes: true,
+              // 是否移除一些空属性：如 <input id="">，这个 id 无值，所以没有意义
+              removeEmptyAttributes: true,
+              // 是否移除全部空格
+              collapseWhitespace: true,
+              // 删除 link 标签的空属性
+              removeStyleLinkTypeAttributes: true,
+              // 要不要对 html 代码里面的 style 代码进行压缩
+              minifyCSS: true,
+              // 是否对 html 里面的 js 进行压缩
+              minifyJS: {
+                mangle: {
+                  // 代码丑化且更换变量
+                  toplevel: true,
+                },
+              },
+            }
+          : false,
     }),
     new CompressionPlugin({
       // 压缩阈值：达到多少才进行压缩
@@ -73,5 +107,7 @@ module.exports = {
       // 包括哪些文件
       // include
     }),
+    // @ts-ignore
+    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime.*\.js/]),
   ],
 }
