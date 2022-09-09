051f489648d4fbc7e2f28438e07224e021873649
feng <feng2860984180@163.com>
2022-09-09 13:54:00 +0800

    gulp 多个任务串行&并行执行

--- 29-gulp/gulpfile.js
+++ 29-gulp/gulpfile.js
@@ -1,3 +1,4 @@
-const gulfileConfig = require('./gulpfile_单个任务')
+// const gulpFileConfig = require('./gulpfile_单个任务.js')
+const gulpFileConfig = require('./gulpfile_多个任务.js')
 
-module.exports = { ...gulfileConfig }
+module.exports = { ...gulpFileConfig }
