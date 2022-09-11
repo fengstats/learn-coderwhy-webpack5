aaf717b5b1b11320cdf01fa1358ec467a7ec3750
feng <feng2860984180@163.com>
2022-09-11 11:02:57 +0800

    gulp 案例之 js 处理

--- 30-gulp-案例/gulpfile.js
+++ 30-gulp-案例/gulpfile.js
@@ -1,5 +1,17 @@
 const { src, dest } = require('gulp')
 const htmlMin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const terser = require('gulp-terser')
+
+const jsTask = () => {
+  return src('./src/js/*.js', {
+    // 以 src 目录基准匹配，生成文件时保留匹配目录结构
+    base: './src',
+  })
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(terser({ mangle: { toplevel: true } }))
+    .pipe(dest('./dist'))
+}
 
 const htmlTask = () => {
   // 记得将流返回出去，否则任务无法正常结束
@@ -13,4 +25,4 @@ const htmlTask = () => {
     .pipe(dest('./dist'))
 }
 
-module.exports = { htmlTask }
+module.exports = { htmlTask, jsTask }
---
+++ 30-gulp-案例/src/js/main.js
@@ -0,0 +1,6 @@
+const sum = (n1, n2) => {
+  return n1 + n2
+}
+
+const result = sum(1, 2)
+console.log(result)
