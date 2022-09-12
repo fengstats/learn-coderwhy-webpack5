8513c461db0056f59e43fa977e2b420e8f350b68
feng <feng2860984180@163.com>
2022-09-12 17:42:08 +0800

    rollup 通过 babel&terser 进行代码转换与压缩

--- 31-rollup/rollup.config.js
+++ 31-rollup/rollup.config.js
@@ -1,5 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs'
-import nodeResolve from '@rollup/plugin-node-resolve'
+// import nodeResolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './src/main.js',
@@ -18,6 +20,12 @@ export default {
     // 可以让我们自己写的代码通过 CommonJS 的方式导出，ES Module 的方式导入
     commonjs(),
     // 可以解决第三方依赖包（node_modules/）引入代码使用，没有将源码打包到输出文件中导致 undefined 的问题
-    nodeResolve(),
+    // nodeResolve(),
+    babel({
+      // 转换 ES6 语法时，多次转化时只使用一个辅助函数
+      babelHelpers: 'bundled',
+    }),
+    // 对代码进行压缩/丑化
+    terser(),
   ],
 }
